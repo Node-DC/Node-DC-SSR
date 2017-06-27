4bb87efe05ea1a054fcfd12da895f000d166bf2f
Enrico Galli <enrico.galli@intel.com>
2017-06-26 18:20:43 -0700

    Initial commit (generated by Electrode generator)

---
+++ src/client/components/home.jsx
@@ -0,0 +1,24 @@
+/*
+ * This is a demo component the Eletrode app generator included
+ * to show using Skeleton CSS lib (named base.css) and Redux
+ * store for display HTML elements and managing states.
+ *
+ * To start your own app, please replace or remove these files:
+ *
+ * - this file (home.jsx)
+ * - demo-buttons.jsx
+ * - demo-pure-states.jsx
+ * - demo-states.jsx
+ * - reducers/index.jsx
+ * - styles/*.css
+ *
+ */
+
+import React from 'react';
+import '../styles/normalize.css';
+import custom from '../styles/custom.css';
+
+export default () =>
+  <div className={custom.container}>
+    {/**/}
+  </div>;
